75f6ad5060a2a176ce541fc2884b1ca63bc9dc7f
Mathieu <43028240+ChristensenMathieu@users.noreply.github.com>
2019-04-30 14:53:24 +0200

    Test Update

--- routes.php
+++ routes.php
@@ -5,7 +5,8 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{table:patients|personnes|badges|infirmieres|visites}]', function (Request $request, Response $response, array $args) {
+$app->get('/{[table:patients|personnes|badges|infirmieres|visites}]', function (Request $request, Response $response, array $args) {
+    //.preg_replace(RegEx, New, Variable)
     $sqlRequest = 'SELECT * FROM '.preg_replace('/s$/', '', $args['table']);
     $retour = $this->db->query($sqlRequest);
     $json = array();
@@ -18,9 +19,9 @@ $app->get('/[{table:patients|personnes|badges|infirmieres|visites}]', function (
     //var_dump($sqlRequest);
 });
 
-$app->get('[/{table:patients|personnes|badges|infirmieres|visites}/{id:\d*}]', function (Request $request, Response $response, array $args) {
-
-    $sqlRequest = 'SELECT * FROM '.preg_replace('/s$/', '', $args['table']).' WHERE id='.$args['id'];
+$app->get('[/{table:patient|personne|badge|infirmiere|visite}/{id:\d*}]', function (Request $request, Response $response, array $args) {
+    
+    $sqlRequest = 'SELECT * FROM '.$args['table'].' WHERE id='.$args['id'];
     $retour = $this->db->query($sqlRequest);
     $json= array();
     foreach ($retour as $row) {
@@ -28,16 +29,19 @@ $app->get('[/{table:patients|personnes|badges|infirmieres|visites}/{id:\d*}]', f
     }
     $json = json_encode($json);
     return $json;
-    var_dump($sqlRequest);
+    //var_dump($sqlRequest);
 
 });
 
 $app->get('/connect', function (Request $request, Response $response, array $args) {
-    
+    $truc = $request->getParams();
+    foreach ($truc as $key=>$value) {
+        echo $key;
+        echo $value;
+    }
     $sqlRequest = ' SELECT * 
                     FROM personne_login pl, personne p
-                    where pl.id = p.id
-                ';
+                    where pl.id = p.id';
     $retour = $this->db->query($sqlRequest); 
     $json['status'] = false;
     foreach ($retour as $row) {
@@ -45,34 +49,25 @@ $app->get('/connect', function (Request $request, Response $response, array $arg
             $json['status'] = true;
             $json['personne']=$row;
         }
-        
     }
     //var_dump($json);
     $json = json_encode($json);
     return $json;
-
 });
 
-$app->post('/personne/patient', function (Request $request, Response $response, array $args) {
-    $params = $request->getParsedBody();
-    $t = array('nom'=>'null', 'prenom'=>'null', 'sexe'=>'null', 'date_naiss'=>'null', 'date_deces'=>'null', 'ad1'=>'null', 'ad2'=>'null', 'cp'=>'null', 'ville'=>'null', 'tel_fixe'=>'null', 'tel_port'=>'null', 'mail'=>'null');
-    /*foreach($t as $key=>$value)
-    {
-        if(isset($params[$key]))
-        {
-            $t[$key]=$params[$key];
-
+$app->put('[/personnes/{id:\d*}]', function (Request $request, Response $response, array $args){
+    $sqlRequest = 'UPDATE personne Set ';
+    $retour = $request->getParams();
+    $i = 0;
+    foreach($retour as $paramerte=>$valeur) {
+        $i ++;
+        $sqlRequest += $paramerte.' = '.$valeur;
+        if (isset($retour[$i])) {
+            $sqlRequest += ' AND ';
+        } else {
+            $sqlRequest += ' WHERE id = '.$args['id'].';';
         }
-    }*/
-
-    $sqlRequest =   'INSERT INTO personne (nom, prenom, sexe, date_naiss, date_deces, ad1, ad2, cp, ville, tel_fixe, tel_port, mail)
-                     VALUES ('. $t['nom'].','. $t['prenom'].','. $t['sexe'].','. $t['date_naiss'].','. $t['date_deces'].','. $t['ad1'].','. $t['ad2'].','. $t['cp'].','. $t['ville'].','. $t['tel_fixe'].','. $t['tel_port'].','. $t['mail'].')';
-    //$retour = $this->db->query($sqlRequest); 
-    return var_dump($params);
-
-
-
+    }
+    var_dump($retour);
+    dump($sqlRequest);
 });
-
-
-
